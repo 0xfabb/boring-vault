4bff0dfc02a32bab0402f48766fd0e9a9aeb1d20
0xfabb <vanshdev2305@gmail.com>
2025-08-15 10:07:35 +0530

    feat: Added core strategy contract

---
+++ test/veda/vedavault.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import {Test} from "forge-std/Test.sol";
+import {VedaVault} from "src/veda/VedaLoopingStrategy.sol";
+import {ERC20} from "@solmate/tokens/ERC20.sol";
+import {Auth, Authority} from "@solmate/auth/Auth.sol";
+
